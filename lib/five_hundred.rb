445afc85e3e23e7da0eb6ea50a4fcfaac564ab45
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-10-17 17:56:58 +0500

    500px search

---
+++ lib/five_hundred.rb
@@ -0,0 +1,25 @@
+class FiveHundred
+  def initialize(api_key)
+    @api_key = api_key
+    @api_url = 'https://api.500px.com/v1'
+  end
+
+  def search(term, size = 440)
+    term = term.strip.downcase
+    url = "#{@api_url}/photos/search"
+    params = { consumer_key: @api_key, term: term, image_size: size }
+    res = RestClient.get url, { accept: :json, params: params }
+    photos = JSON.parse(res)['photos']
+    photo = photos[rand photos.length]['images'][0]
+    download photo
+  end
+
+  protected
+    def download(photo)
+      in_file = open photo['url']
+      out_file = Tempfile.new [File.basename(in_file), ".#{photo['format']}"]
+      out_file.write in_file.read
+      out_file.seek(0)
+      out_file
+    end
+end
