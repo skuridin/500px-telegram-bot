6e99957819f867b2600c5b7da7d58ed64768391b
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-10-17 16:34:45 +0500

    Add send_photo method

--- lib/speaker.rb
+++ lib/speaker.rb
@@ -6,8 +6,22 @@ class Speaker
   def send_message(chat_id, text, reply_id = nil)
     params = { chat_id: chat_id, text: text }
     params[:reply_to_message_id] = reply_id unless reply_id.nil?
-    headers = { content_type: :json, accept: :json }
-    res = RestClient.post "#{@api_url}/sendMessage", params, headers
-    p res
+    post "#{@api_url}/sendMessage", params
   end
+
+  def send_photo(chat_id, file, reply_id = nil)
+    params = { chat_id: chat_id, photo: file }
+    params[:reply_to_message_id] = reply_id unless reply_id.nil?
+    post "#{@api_url}/sendPhoto", params
+  end
+
+  protected
+    def post(url, params)
+      headers = { content_type: :json, accept: :json }
+      begin
+        RestClient.post url, params, headers
+      rescue RestClient::Exception => e
+        puts e.message
+      end
+    end
 end
